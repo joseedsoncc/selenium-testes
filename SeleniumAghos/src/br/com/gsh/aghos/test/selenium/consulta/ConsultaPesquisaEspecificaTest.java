9a626d2c95b4eb1cef7a39ffc6e317a9eb1ec873
jose.edson <joseedsoncc@hotmail.com>
2014-05-28 12:51:00 -0300

    correções do teste ConsultaPesquisaEspecificaTest

--- SeleniumAghos/src/br/com/gsh/aghos/test/selenium/consulta/ConsultaPesquisaEspecificaTest.java
+++ SeleniumAghos/src/br/com/gsh/aghos/test/selenium/consulta/ConsultaPesquisaEspecificaTest.java
@@ -35,46 +35,19 @@ public class ConsultaPesquisaEspecificaTest extends SeleniumTest {
 	    driver.findElement(By.id("nome")).sendKeys("santa casa de misericordia de cuiaba teste");
 	    driver.findElement(By.id("btnPesquisarUnidade")).click();
 	    driver.findElement(By.xpath("//tbody[@id='resultadosUnidadeDeSaude']/tr/td[3]")).click();
+	    driver.findElement(By.name("filtro.atendimento.id")).clear();
+	    driver.findElement(By.name("filtro.atendimento.id")).sendKeys("130");
 	    new Select(driver.findElement(By.id("prioridades"))).selectByVisibleText("Normal");
 	    new Select(driver.findElement(By.id("tipoConsulta"))).selectByVisibleText("RETORNO");
 	    new Select(driver.findElement(By.id("estados"))).selectByVisibleText("CANCELADO");
-	    driver.findElement(By.cssSelector("#consultarSubEspecialidade > span.add-on > span.icon-white.icon-search")).click();
-	    driver.findElement(By.id("nomeSubEspecialidade")).clear();
-	    driver.findElement(By.id("nomeSubEspecialidade")).sendKeys("acidentes");
-	    driver.findElement(By.xpath("//tbody[@id='resultadosSubEspecialidade']/tr/td[2]")).click();
-	    //driver.findElement(By.name("selectSubEspecialidade")).toString("148:ACIDENTES").click();//148:ACIDENTES   selectSubEspecialidade
-	    
-	    driver.findElement(By.xpath("//form[@id='formPesquisa']/fieldset/div[12]/div[2]/div/div/span/span")).click();
-	    
 	    driver.findElement(By.id("buscar")).click();
-	    driver.findElement(By.xpath("(//a[contains(text(),'Ações')])[2]")).click();
-	    driver.findElement(By.xpath("(//a[contains(text(),'Visualizar')])[2]")).click();
+	    Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+	    driver.findElement(By.linkText("Ações")).click();
+	    driver.findElement(By.linkText("Visualizar")).click();
+	    Thread.sleep(SeleniumConfig.TIMER_SLEEP + 6000);
 	    driver.findElement(By.linkText("Voltar")).click();
-		
-		
-//		driver.findElement(By.id("buscar")).click();
-//		driver.findElement(By.linkText("6")).click();
-//		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-//		driver.findElement(By.xpath("(//a[contains(text(),'Ações')])[3]")).click();
-//	    driver.findElement(By.xpath("(//a[contains(text(),'Cupom\n				Solicitação')])[3]")).click();
-//	    Thread.sleep(SeleniumConfig.TIMER_SLEEP + 3000);
-//	    driver.findElement(By.cssSelector("#relatorioModal > div.modal-header > button.close")).click();
-//	    Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		
+	    Thread.sleep(SeleniumConfig.TIMER_SLEEP);
 	}
-	
-//	public void testAdicionaUpsIndicacaoConsulta() throws Exception {
-//	    driver.findElement(By.linkText("5")).click();
-//	    driver.findElement(By.linkText("Ações")).click();
-//	    driver.findElement(By.linkText("UPS Indicação Consulta")).click();
-//	    driver.findElement(By.cssSelector("span.icon-white.icon-search")).click();
-//	    driver.findElement(By.id("btnPesquisarUnidade")).click();
-//	    driver.findElement(By.xpath("(//input[@name='selectUnidadeDeSaude'])[11]")).click();
-//	    driver.findElement(By.name("nome")).click();
-//	    assertTrue(isElementPresent(By.className("success")));
-//	    driver.findElement(By.linkText("Cancelar")).click();
-//	    Thread.sleep(SeleniumConfig.TIMER_SLEEP + 6000);
-//	}
 	
 	// Elaborar testes para Chrome
 	public void testConsultaPesquisaEspecificaChrome() {
