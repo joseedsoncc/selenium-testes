7ae5a3f91665e84cdc31c5b617ce658ec8e34700
jose.edson <joseedsoncc@hotmail.com>
2014-05-29 11:24:12 -0300

    alteracao no cadastro devido o dado nao conter no banco atual

--- SeleniumAghos/src/br/com/gsh/aghos/test/selenium/pessoaFisica/PessoaFisicaCadastroTest.java
+++ SeleniumAghos/src/br/com/gsh/aghos/test/selenium/pessoaFisica/PessoaFisicaCadastroTest.java
@@ -15,9 +15,9 @@ public class PessoaFisicaCadastroTest extends SeleniumTest{
 	public void pessoaFisicaTest() throws Exception {
 		login();
 		if (SeleniumConfig.isFirefox()) {
-			cadastroPessoaFisicaFireFox();
-		} else {
 			cadastroPessoaFisica();
+		} else {
+			cadastroPessoaFisicaChrome();
 		}
 		efetuaLogoutDoAghos();
 	}
@@ -31,6 +31,99 @@ public class PessoaFisicaCadastroTest extends SeleniumTest{
 	}
 
 	private void cadastroPessoaFisica() throws Exception {
+		driver.get(SeleniumConfig.SERVER + "/pessoa/fisica/");
+		driver.findElement(By.id("pessoa_nome")).clear();
+		driver.findElement(By.id("pessoa_nome")).sendKeys("RYU KEN MASTER");
+		driver.findElement(By.id("pessoa_apelido")).clear();
+		driver.findElement(By.id("pessoa_apelido")).sendKeys("RYU KEN MASTER");
+		driver.findElement(By.id("pessoa_nomeMae")).clear();
+		driver.findElement(By.id("pessoa_nomeMae")).sendKeys("MAE DO RYU KEN MASTER");
+		driver.findElement(By.name("pessoa.nomePai")).clear();
+		driver.findElement(By.name("pessoa.nomePai")).sendKeys("PAI DO RYU KEN MASTER");
+		driver.findElement(By.id("pessoa_nascimento")).clear();
+		driver.findElement(By.id("pessoa_nascimento")).sendKeys("15101979");
+		new Select(driver.findElement(By.id("pessoa_sexo"))).selectByVisibleText("Masculino");
+		new Select(driver.findElement(By.name("pessoa.estadoCivil.id"))).selectByVisibleText("SOLTEIRO(A)");
+		new Select(driver.findElement(By.id("selectRaca"))).selectByVisibleText("SEM INFORMAÇÃO");
+
+		driver.findElement(By.linkText("Naturalidade")).click();
+		driver.findElement(By.linkText("Naturalidade")).click();
+		new Select(driver.findElement(By.id("ufNaturalidade"))).selectByVisibleText("CE");
+		new Select(driver.findElement(By.id("municipioNaturalidade"))).selectByVisibleText("FORTALEZA");
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Município de Trabalho")).click();
+		driver.findElement(By.linkText("Município de Trabalho")).click();
+		driver.findElement(By.cssSelector("#consultarMunicipioTrabalho > span.add-on.data > span.icon-white.icon-search")).click();
+		driver.findElement(By.id("btnPesquisarMunicipioTrabalho")).click();
+		driver.findElement(By.name("selectMunicipioTrabalho")).click();
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Nacionalidade")).click();
+		driver.findElement(By.linkText("Nacionalidade")).click();
+		new Select(driver.findElement(By.name("pessoa.nacionalidade.id"))).selectByVisibleText("BRASIL");
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.id("enderecoCollapse")).click();
+		driver.findElement(By.id("enderecoCollapse")).click();
+		driver.findElement(By.id("cep")).clear();
+		driver.findElement(By.id("cep")).sendKeys("60540060");
+		driver.findElement(By.id("cep")).getAttribute("value");
+		driver.findElement(By.id("numeroResidencia")).clear();
+		driver.findElement(By.id("numeroResidencia")).sendKeys("642");
+		new Select(driver.findElement(By.name("pessoa.zona.id"))).selectByVisibleText("URBANA");
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Ocupação")).click();
+		driver.findElement(By.linkText("Ocupação")).click();
+		driver.findElement(By.cssSelector("#consultarOcupacao > span.add-on.data > span.icon-white.icon-search")).click();
+		driver.findElement(By.cssSelector("#btnPesquisarOcupacao > span.icon-white.icon-search")).click();
+		driver.findElement(By.name("selectOcupacao")).click();
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+
+		driver.findElement(By.linkText("Ocupação Auxiliar")).click();
+		driver.findElement(By.linkText("Ocupação Auxiliar")).click();
+		driver.findElement(By.cssSelector("#consultarOcupacaoAuxiliar > span.add-on.data > span.icon-white.icon-search")).click();
+		driver.findElement(By.cssSelector("#btnPesquisarOcupacaoAuxiliar > span.icon-white.icon-search")).click();
+		driver.findElement(By.xpath("//tbody[@id='resultadosOcupacaoAuxiliar']/tr/td[2]")).click();
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Responsáveis")).click();
+		driver.findElement(By.linkText("Responsáveis")).click();
+		driver.findElement(By.cssSelector("#consultarResponsavel1 > span.add-on.data > span.icon-white.icon-search")).click();
+		driver.findElement(By.id("btnPesquisarPessoa")).click();
+		driver.findElement(By.name("selectPessoa")).click();
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Domicílio")).click();
+		driver.findElement(By.linkText("Domicílio")).click();
+		new Select(driver.findElement(By.id("energia"))).selectByVisibleText("Sim");
+		new Select(driver.findElement(By.id("tipoAbastecimentoAgua"))).selectByVisibleText("POÇO OU NASCENTE");
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		
+		driver.findElement(By.linkText("Telefones")).click();
+		driver.findElement(By.linkText("Telefones")).click();
+		new Select(driver.findElement(By.id("tipoDeTelefone"))).selectByVisibleText("RESIDENCIAL");
+
+		driver.findElement(By.id("campoTelefone")).clear();
+		driver.findElement(By.id("campoTelefone")).sendKeys("55555555555");
+		driver.findElement(By.id("adicionarTelefone")).click();
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+
+		driver.findElement(By.linkText("E-mails")).click();
+		driver.findElement(By.linkText("E-mails")).click();
+		driver.findElement(By.id("campoEmail")).clear();
+		driver.findElement(By.id("campoEmail")).sendKeys("emaildoryu@streetfighter.com");
+		driver.findElement(By.id("adicionarEmail")).click();
+
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
+		assertTrue(isElementPresent(By.id("mensagens")));
+		assertTrue(isElementPresent(By.className("success")));
+	}
+
+	private void cadastroPessoaFisicaChrome() throws Exception {
 		driver.get(SeleniumConfig.SERVER + "/pessoa/fisica/");
 		driver.findElement(By.id("pessoa_nome")).clear();
 		driver.findElement(By.id("pessoa_nome")).sendKeys("RYU KEN MASTER");
@@ -124,103 +217,7 @@ public class PessoaFisicaCadastroTest extends SeleniumTest{
 		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
 		assertTrue(isElementPresent(By.className("success")));
 	}
-
-	private void cadastroPessoaFisicaFireFox() throws Exception {
-		driver.get(SeleniumConfig.SERVER + "/pessoa/fisica/");
-		driver.findElement(By.id("pessoa_nome")).clear();
-		driver.findElement(By.id("pessoa_nome")).sendKeys("RYU KEN MASTER");
-		driver.findElement(By.id("pessoa_apelido")).clear();
-		driver.findElement(By.id("pessoa_apelido")).sendKeys("RYU KEN MASTER");
-		driver.findElement(By.id("pessoa_nomeMae")).clear();
-		driver.findElement(By.id("pessoa_nomeMae")).sendKeys("MAE DO RYU KEN MASTER");
-		driver.findElement(By.name("pessoa.nomePai")).clear();
-		driver.findElement(By.name("pessoa.nomePai")).sendKeys("PAI DO RYU KEN MASTER");
-		driver.findElement(By.id("pessoa_nascimento")).clear();
-		driver.findElement(By.id("pessoa_nascimento")).sendKeys("15101979");
-		new Select(driver.findElement(By.id("pessoa_sexo"))).selectByVisibleText("Masculino");
-		new Select(driver.findElement(By.name("pessoa.estadoCivil.id"))).selectByVisibleText("SOLTEIRO(A)");
-		new Select(driver.findElement(By.id("selectRaca"))).selectByVisibleText("SEM INFORMAÇÃO");
-
-		driver.findElement(By.linkText("Naturalidade")).click();
-		driver.findElement(By.linkText("Naturalidade")).click();
-		driver.findElement(By.linkText("Naturalidade")).click();
-		driver.findElement(By.linkText("Naturalidade")).click();
-		new Select(driver.findElement(By.id("ufNaturalidade"))).selectByVisibleText("CE");
-		new Select(driver.findElement(By.id("municipioNaturalidade"))).selectByVisibleText("FORTALEZA");
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Município de Trabalho")).click();
-		driver.findElement(By.linkText("Município de Trabalho")).click();
-		driver.findElement(By.linkText("Município de Trabalho")).click();
-		driver.findElement(By.linkText("Município de Trabalho")).click();
-
-		driver.findElement(By.cssSelector("#consultarMunicipioTrabalho > span.add-on.data > span.icon-white.icon-search")).click();
-		driver.findElement(By.id("btnPesquisarMunicipioTrabalho")).click();
-		driver.findElement(By.name("selectMunicipioTrabalho")).click();
-
-		driver.findElement(By.linkText("Nacionalidade")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Nacionalidade")).click();
-		new Select(driver.findElement(By.name("pessoa.nacionalidade.id"))).selectByVisibleText("BRASIL");
-
-		driver.findElement(By.id("enderecoCollapse")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.id("enderecoCollapse")).click();
-		driver.findElement(By.id("cep")).clear();
-		driver.findElement(By.id("cep")).sendKeys("60540060");
-		driver.findElement(By.id("cep")).getAttribute("value");
-		driver.findElement(By.id("numeroResidencia")).clear();
-		driver.findElement(By.id("numeroResidencia")).sendKeys("642");
-		new Select(driver.findElement(By.name("pessoa.zona.id"))).selectByVisibleText("URBANA");
-
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Ocupação")).click();
-		driver.findElement(By.linkText("Ocupação")).click();
-		driver.findElement(By.linkText("Ocupação")).click();
-		driver.findElement(By.linkText("Ocupação")).click();
-		driver.findElement(By.cssSelector("#consultarOcupacao > span.add-on.data > span.icon-white.icon-search")).click();
-		driver.findElement(By.cssSelector("#btnPesquisarOcupacao > span.icon-white.icon-search")).click();
-		driver.findElement(By.name("selectOcupacao")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-
-		driver.findElement(By.linkText("Ocupação Auxiliar")).click();
-		driver.findElement(By.linkText("Ocupação Auxiliar")).click();
-		driver.findElement(By.cssSelector("#consultarOcupacaoAuxiliar > span.add-on.data > span.icon-white.icon-search")).click();
-		driver.findElement(By.cssSelector("#btnPesquisarOcupacaoAuxiliar > span.icon-white.icon-search")).click();
-		driver.findElement(By.xpath("//tbody[@id='resultadosOcupacaoAuxiliar']/tr/td[2]")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Responsáveis")).click();
-		driver.findElement(By.linkText("Responsáveis")).click();
-		driver.findElement(By.cssSelector("#consultarResponsavel1 > span.add-on.data > span.icon-white.icon-search")).click();
-		driver.findElement(By.id("btnPesquisarPessoa")).click();
-		driver.findElement(By.name("selectPessoa")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Domicílio")).click();
-		driver.findElement(By.linkText("Domicílio")).click();
-		new Select(driver.findElement(By.id("energia"))).selectByVisibleText("Sim");
-		new Select(driver.findElement(By.id("tipoAbastecimentoAgua"))).selectByVisibleText("REDE PÚBLICA");
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		driver.findElement(By.linkText("Telefones")).click();
-		driver.findElement(By.linkText("Telefones")).click();
-		new Select(driver.findElement(By.id("tipoDeTelefone"))).selectByVisibleText("RESIDENCIAL");
-
-		driver.findElement(By.id("campoTelefone")).clear();
-		driver.findElement(By.id("campoTelefone")).sendKeys("55555555555");
-		driver.findElement(By.id("adicionarTelefone")).click();
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-
-		driver.findElement(By.linkText("E-mails")).click();
-		driver.findElement(By.linkText("E-mails")).click();
-		driver.findElement(By.id("campoEmail")).clear();
-		driver.findElement(By.id("campoEmail")).sendKeys("emaildoryu@streetfighter.com");
-		driver.findElement(By.id("adicionarEmail")).click();
-
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-		Thread.sleep(SeleniumConfig.TIMER_SLEEP);
-		assertTrue(isElementPresent(By.id("mensagens")));
-		assertTrue(isElementPresent(By.className("success")));
-	}
-
+	
 	private void efetuaLogoutDoAghos() throws Exception {
 		driver.findElement(By.xpath("//div[@id='menuUsuarios']/button[2]")).click();
 		driver.findElement(By.linkText("Sair do Sistema")).click();
